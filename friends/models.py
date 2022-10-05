b97c8f7e1eb915395e4228af471b46f8c17079ba
DanielAvilez1 <104475313+DanielAvilez1@users.noreply.github.com>
2022-10-04 19:03:14 -0700

    new
    added photo

--- friends/models.py
+++ friends/models.py
@@ -8,6 +8,7 @@ class Share(models.Model):
     subtitle = models.CharField(max_length=256)
     body= models.TextField()
     created_on= models.DateTimeField(auto_now_add=True)
+    image =models.ImageField(null=True, blank=True)
     author =models.ForeignKey(
         get_user_model(),
         on_delete=models.CASCADE
